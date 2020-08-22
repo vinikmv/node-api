1902e1fda7735a47fd1c73b8895f6e04a16ed24b
Vinicius Vicente <viniciuskmv@gmail.com>
2020-08-22 19:28:20 -0300

    refactor: move validation to protocols folder and group validation imports in a single file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -3,7 +3,7 @@ import { MissingParamError, ServerError } from '../../errors'
 import { AccountModel, AddAccountModel, AddAccount } from './signup-protocols'
 import { HttpRequest } from '../../protocols'
 import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
-import { Validation } from '../../helpers/validators/validation'
+import { Validation } from '../../protocols/validation'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
