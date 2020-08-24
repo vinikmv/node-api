2e67290d28f4886b2d62c1e2be1012ed5494991b
Vinicius Vicente <viniciuskmv@gmail.com>
2020-08-24 12:12:23 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -14,7 +18,7 @@ describe('Login Routes', () => {
   // Remover registros da tabela para evitar a influência de lixo nos testes
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +31,24 @@ describe('Login Routes', () => {
           email: 'viniciuskmv@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Vinicius',
+        email: 'viniciuskmv@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'viniciuskmv@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
