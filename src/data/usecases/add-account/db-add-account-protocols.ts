b5ad48f2a94570c402e78a4b71be9473c0c7254c
Vinicius Vicente <viniciuskmv@gmail.com>
2020-08-26 09:41:48 -0300

    refactor: rename folder to keep consistency

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,5 +1,5 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
 export * from '../../protocols/cryptography/hasher'
-export * from '../../protocols/db/account/add-account-repository'
-export * from '../../protocols/db/account/load-account-by-email-repository'
+export * from '../../protocols/db/add-account/add-account-repository'
+export * from '../../protocols/db/add-account/load-account-by-email-repository'
