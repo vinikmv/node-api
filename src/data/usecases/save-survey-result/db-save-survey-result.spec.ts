7c718d258bd2adfa6c79d2d3c5f9259c4f2f1d8f
Vinicius Vicente <viniciuskmv@gmail.com>
2020-08-31 10:50:45 -0300

    test: ensure DbSaveSurveyResult throw if SaveSurveyResultRepository throws

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -36,7 +36,7 @@ const makeSut = (): SutTypes => {
   }
 }
 
-describe('DbSavesurveyResult Usecase', () => {
+describe('DbSaveSurveyResult Usecase', () => {
   beforeAll(() => {
     MockDate.set(new Date())
   })
@@ -51,5 +51,12 @@ describe('DbSavesurveyResult Usecase', () => {
     const surveyResultData = makeFakeSurveyResultData()
     await sut.save(surveyResultData)
     expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
+  })
+
+  test('Should throw if SaveSurveyResultRepository throws', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(saveSurveyResultRepositoryStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.save(makeFakeSurveyResultData())
+    await expect(promise).rejects.toThrow()
   })
 })
