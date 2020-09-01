1da7c08a84630e387b4215e45d0dfe8cdc451ec5
Vinicius Vicente <viniciuskmv@gmail.com>
2020-09-01 09:17:15 -0300

    fix: removed duplicated code that was opening mongo two times on tests

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -42,46 +42,36 @@ describe('Survey Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  describe('Survey Routes', () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    describe('PUT /surveys/:surveyId/results', () => {
-      test('Should return 403 on save survey without accessToken', async () => {
-        await request(app)
-          .put('/api/surveys/any_id/results')
-          .send({
-            answer: 'any_answer'
-          })
-          .expect(403)
-      })
-
-      test('Should return 200 on save survey with accessToken', async () => {
-        const accessToken = await makeAccessToken()
-        const res = await surveyCollection.insertOne({
-          question: 'Question',
-          answers: [{
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          }, {
-            answer: 'Answer 2'
-          }],
-          date: new Date()
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
         })
-        const id: string = res.ops[0]._id
-        await request(app)
-          .put(`/api/surveys/${id}/results`)
-          .set('x-access-token', accessToken)
-          .send({
-            answer: 'Answer 1'
-          })
-          .expect(200)
+        .expect(403)
+    })
+
+    test('Should return 200 on save survey with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
       })
+      const id: string = res.ops[0]._id
+      await request(app)
+        .put(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
