a7248c2647007d758aeadc547f581092e31a9b3c
Vinicius Vicente <viniciuskmv@gmail.com>
2020-09-01 16:01:38 -0300

    feat: add swagger ui

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Node API with Clean Architecture',
+    description: 'API do curso de Node para realizar enquetes',
+    version: '1.0.0'
+  }
+}
